2a2728f2f48f1113db3b0b93fa1f3b928224e8cd
chandrakanth <chandrakanthmullangi.developer@gmail.com>
2023-06-07 03:40:52 +0000

    first commit

---
+++ src/components/Products/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import './index.css'
+
+const Products = () => (
+  <div className="products">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      alt="products"
+      className="products-image"
+    />
+  </div>
+)
+
+export default Products
